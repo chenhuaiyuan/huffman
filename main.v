205146e380d4321ac96d24e1e8da59580f953e5c
chenhuaiyuan <343938938@qq.com>
2021-04-27 21:40:15 +0800

    优化

--- main.v
+++ main.v
@@ -4,12 +4,11 @@ import huffman
 
 const (
 	file_path  = './test.jpg'
-	compress   = './test.zz'
-	file1_path = './test1.jpg'
+	file1_path = './test.zz'
+	file2_path = './test1.jpg'
 )
 
 fn main() {
-	// folder := os.dir(os.args[0])
-	huffman.encode_file(file_path, compress) ?
-	huffman.decode_file(compress, file1_path) ?
+	huffman.encode_file(file_path, file1_path) ?
+	huffman.decode_file(file1_path, file2_path) ?
 }
--- v.mod
+++ v.mod
@@ -1,6 +1,6 @@
 Module {
 	name: 'huffman'
-	description: 'vlang huffman'
+	description: 'v语言实现的Huffman'
 	version: '0.0.1'
 	license: 'MIT'
 	dependencies: []
